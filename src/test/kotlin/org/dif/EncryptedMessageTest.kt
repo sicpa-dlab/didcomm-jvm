06d9e7e0d5af93366d18c505267845d9d6c7b4d3
Alexander Martynov <alexander.martynov@dsr-corporation.com>
2021-09-03 10:34:21 +0300

    updated test vectors

--- src/test/kotlin/org/dif/EncryptedMessageTest.kt
+++ src/test/kotlin/org/dif/EncryptedMessageTest.kt
@@ -7,17 +7,18 @@ import org.dif.mock.BobSecretResolverMock
 import org.dif.mock.DIDDocResolverMock
 import org.dif.model.UnpackParams
 import kotlin.test.Test
+import kotlin.test.assertContentEquals
 import kotlin.test.assertEquals
 
 class EncryptedMessageTest {
 
     @Test
     fun `Test encrypted message test vectors`() {
-        for (test in TEST_VECTORS) {
+        for (tv in TEST_VECTORS) {
             val didComm = DIDComm(DIDDocResolverMock(), BobSecretResolverMock())
 
             val unpacked = didComm.unpack(
-                UnpackParams.Builder(test)
+                UnpackParams.Builder(tv.message)
                     .expectDecryptByAllKeys(true)
                     .build()
             )
@@ -26,6 +27,26 @@ class EncryptedMessageTest {
                 JSONObjectUtils.toJSONString(JWM.PLAINTEXT_MESSAGE.toJSONObject()),
                 JSONObjectUtils.toJSONString(unpacked.message.toJSONObject())
             )
+
+            with(unpacked.metadata) {
+                assertEquals(tv.expectedMetadata.encrypted, encrypted)
+                assertEquals(tv.expectedMetadata.authenticated, authenticated)
+                assertEquals(tv.expectedMetadata.anonymousSender, anonymousSender)
+                assertEquals(tv.expectedMetadata.nonRepudiation, nonRepudiation)
+
+                assertEquals(tv.expectedMetadata.encAlgAnon, encAlgAnon)
+                assertEquals(tv.expectedMetadata.encAlgAuth, encAlgAuth)
+
+                assertEquals(tv.expectedMetadata.encryptedFrom, encryptedFrom)
+                assertContentEquals(tv.expectedMetadata.encryptedTo, encryptedTo)
+
+                assertEquals(tv.expectedMetadata.signAlg, signAlg)
+                assertEquals(tv.expectedMetadata.signFrom, signFrom)
+
+                val expectedSignedMessage = tv.expectedMetadata.signedMessage?.let { true } ?: false
+                val actualSignedMessage = signedMessage?.let { true } ?: false
+                assertEquals(expectedSignedMessage, actualSignedMessage)
+            }
         }
     }
 }
